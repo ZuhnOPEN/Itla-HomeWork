2877ca37a2969ed45eb5c8caae0099c3918c336a
Randolf Vladimir Martinez Beltre <20241725@itla.edu.do>
2025-05-25 11:32:20 -0400

    Adding Homeworks 1 & 2

---
+++ HW 2/Homework 2/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Write("Introduzca el primer numero: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % 2 == 0)
+{
+    Console.WriteLine("El numero es par!");
+} else
+{
+    Console.WriteLine("El numero es impar");
+}
