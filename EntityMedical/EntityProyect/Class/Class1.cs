b6c2ab58a0a97e1c18f296b1932f72cb0d6f832c
OscarD1926 <214184132+OscarD1926@users.noreply.github.com>
2025-07-25 08:41:21 -0400

    Ending up the medical app

---
+++ EntityMedical/EntityProyect/Class/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace EntityProyect.Class
+{
+    public class user
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Cedula {  get; set; }
+        public string Email { get; set; }
+        public string lastName { get; set; }
+        public string Numero { get; set; }
+        public bool bestFriend { get; set; }
+
+    }
+
+    public class userContext : DbContext
+    {
+        public DbSet<user> users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = Contacts; Trusted_Connection = True; ");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ EntityMedical/EntityProyect/Program.cs
@@ -0,0 +1,175 @@
+﻿using EntityProyect.Class;
+using System.Text;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        using (var context = new userContext())
+        {
+            int option = 0;
+            context.Database.EnsureCreated();
+            do
+            {
+                Console.WriteLine("═════════════════════════════════════════════════════════════════════════════════════════════════════════");
+                Console.WriteLine("1. Añadir paciente 2. Ver pacientes 3. Editar pacientes 4. Buscar pacientes 5. Eliminar paciente 6. Salir");
+                Console.WriteLine("═════════════════════════════════════════════════════════════════════════════════════════════════════════");
+
+                option = Convert.ToInt32(Console.ReadLine());
+           
+                switch (option)
+                {
+                    //añadir
+                    case 1:
+                        {
+                            
+                            Console.WriteLine("Escribe tu nombre: ");
+                            string fname = Convert.ToString(Console.ReadLine());
+
+                            Console.WriteLine("Introduce la cedula del paciente: ");
+                            int addCedula = Convert.ToInt32(Console.ReadLine());
+
+                            Console.WriteLine("Escribe tu apellido: ");
+                            string lname = Convert.ToString(Console.ReadLine());
+
+                            Console.WriteLine("Escribe tu correo: ");
+                            string correo = Convert.ToString(Console.ReadLine());
+
+                            Console.WriteLine("Escribe tu numero de telefono");
+                            string numero = Convert.ToString(Console.ReadLine());
+
+                            Console.WriteLine("Es tu mejor amigo? ");
+                            bool bestie = Convert.ToBoolean(Console.ReadLine());
+
+                            var usr1 = new user() { Name = fname, Cedula = addCedula, lastName = lname, Email = correo, Numero = numero, bestFriend = bestie };
+
+                            context.users.Add(usr1);
+                            context.SaveChanges();
+                        }
+                        break;
+                    //ver
+                    case 2:
+                        { 
+                            if(context.users == null)
+                            {
+                                Console.WriteLine("No hay usuarios en la base de datos");
+                            }else
+                            {
+                                foreach (var u in context.users)
+                                {
+                                    Console.WriteLine("════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════");
+                                    Console.WriteLine($"ID: {u.Id} | Nombre: {u.Name}  | Cedula: {u.Cedula} | Apellido: {u.lastName} | Correo: {u.Email} | Telefono: {u.Numero} | Es tu mejor amigo? {u.bestFriend}");
+                                    Console.WriteLine("════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════");
+
+                                }
+                            }
+                                
+                        }break;
+                        //editar usuarios
+                    case 3:
+                        {
+                            using (var edit = new userContext())
+                            {
+                                Console.WriteLine("Selecciona el ID a editar: ");
+                                int id = Convert.ToInt32(Console.ReadLine());
+
+                                Console.WriteLine("Introduzca el nuevo nombre: ");
+                                string editName = Convert.ToString(Console.ReadLine());
+
+                                Console.WriteLine("Introduzca el nuevo apellido: ");
+                                string editApellido = Convert.ToString(Console.ReadLine());
+
+                                Console.WriteLine("Edite el correo del usuario:");
+                                string editEmail = Convert.ToString(Console.ReadLine());
+
+                                Console.WriteLine("Edita el numero telefonico del usuario: ");
+                                string editNumb = Convert.ToString(Console.ReadLine());
+
+                                Console.WriteLine("Cambia la relacion entre el usuario y tu");
+                                bool editFriend = Convert.ToBoolean(Console.ReadLine());
+
+                                var usrEdit = new user() {Id = id, Name = editName, lastName = editApellido, Email = editEmail, Numero = editNumb, bestFriend = editFriend };
+
+                                context.UpdateRange(usrEdit);
+                                context.SaveChanges();
+
+                            }
+                        }break;
+                        //buscar pacientes
+                    case 4:
+                        {
+                            Console.WriteLine("Introduce el nombre a buscar: ");
+                            int searchID = Convert.ToInt32(Console.ReadLine());
+
+                            using (var search = new userContext())
+                            {
+                               var found = search.users.Find(searchID);
+                                Console.WriteLine("═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════");
+                                Console.WriteLine($"ID: {found.Id} | Nombre: {found.Name} | Cedula: {found.Cedula} | Apellido {found.lastName} | Correo: {found.Email} | Numero: {found.Numero} | Estado: {found.bestFriend}");
+                                Console.WriteLine("═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════");
+
+                            }
+                        }
+                        break;
+
+                        //elimiar usuarios
+                    case 5:
+                        {
+                            Console.WriteLine("Introduce el ID del paciente a eliminar: ");
+                            int delID = Convert.ToInt32(Console.ReadLine());
+
+                            var delUser = new user() { Id = delID};
+
+                            if (decide == 1)
+                            {
+                                using (var delete = new userContext())
+                                { 
+                                    delete.Remove<user>(delUser);
+                                    delete.SaveChanges();
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("No se eliminara este usuario.");
+                            }
+
+                        
+                        }break;
+                }
+            } while (option != 6);
+
+
+        }
+    }
+}
+
+//This is the biggest piece of bullshit i ever wrote
+
+
+
+
+//⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠈⠈⠉⠉⠈⠈⠈⠉⠉⠉⠉⠉⠉⠉⠉⠙⠻⣄⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠓⣄⠀⠀⢀⠀⢀⣀⣤⠄⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢷⣉⣩⣤⠴⠶⠶⠒⠛⠛⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⣴⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣧⠤⠶⠒⠚⠋⠉⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⢀⣾⡍⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣫⣭⣷⠶⢶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠶⠶⠖⠚⠛⠛⣹⠏⠀⠀⠀⠀⠀⠀⠀⠀⠴⠛⠛⠉⡁⠀⠀⠙⠻⣿⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⢠⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⡷⠷⢿⣦⣤⣈⡙⢿⣿⢆⣴⣤⡄⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⣠⣤⡀⣸⡄⠀⠀⠀⠀⠀⠀⠀⢀⣤⣿⣿⣟⣩⣤⣴⣤⣌⣿⣿⣿⣦⣹⣿⢁⣿⣿⣄⣀⡀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⢠⣿⠋⠻⢿⡁⠀⠀⠀⠀⠀⠀⠀⠀⢸⡿⠿⠛⢦⣽⣿⣿⢻⣿⣿⣿⣿⠋⠁⠘⣿⣿⣿⣿⣿⣿⣼⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⢸⣿⠁⠀⠀⠙⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠒⠿⣿⣯⣼⣿⡿⠟⠃⠀⠀⠀⣿⣿⣿⣿⣿⡛⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⢸⣧⣴⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣺⠟⠃⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⢁⣀⣀⣀⣀⣀⣠⣀⣀⢀⢀⢀
+//⠀⠀⢿⠿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⡆⠙⠛⠛⠙⢻⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⡇⠀⠘⠃⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⡟⢿⣿⣆⠀⣸⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢄⡼⠁⢀⣀⡀⠀⠀⠀⣦⣄⠀⣠⡄⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣷⣬⢻⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣧⣰⣿⡿⠿⠦⢤⣴⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⣿⣸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠛⠛⠒⣿⣿⣿⡿⠟⠹⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⣿⠸⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡖⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⡿⣾⣿⣸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣆⣀⣀⣤⣴⣶⣶⣾⣿⣷⣦⣴⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⡇⣿⣿⡛⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⢾⡟⠛⠛⠻⠛⠛⠛⠿⠿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⠓⢁⣬⣿⠇⠀⠀⠀⠀⠀⢠⡀⠀⠀⠀⠀⠀⢰⡿⣻⠇⠀⠀⠀⠀⠀⣠⣶⣶⣶⣶⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⢐⣯⠞⠁⠀⠀⠀⠀⠀⠀⣄⠱⣄⠀⠀⠀⠀⠸⡧⠟⠆⠀⠀⠀⠀⠘⠿⢿⠿⠿⣿⡿⣿⠃⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⡾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠘⢦⡈⠂⠀⠑⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢠⣿⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠒⡄⠀⠀⠑⠄⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣦⣦⣼⡏⠳⣜⢿⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⢠⣷⣦⣤⣀⣀⣀⣴⣿⣿⣿⣿⣿⡿⠻⠆⠸⣎⣧⠀⠈⠙⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣄⠀⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⣠⡄⠀⣿⢹⡇⢸⡀⠀⠈⠻⢿⣿⣿⣿⣿⣿⣿
